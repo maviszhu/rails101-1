b9c14e3447902022d09045310bbeba639320c295
maviszhu <maviszhu1987@gmail.com>
2016-12-31 11:42:08 +0800

    add account#posts/edit&delete

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,40 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @posts = current_user.posts
-  end 
+    @posts = current_user.posts.recent
+  end
+  def edit
+    find_post_and_check_permission
+  end
+
+  def update
+    find_post_and_check_permission
+    if @post.update(post_params)
+      redirect_to account_posts_path,notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    find_post_and_check_permission
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to account_posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def find_post_and_check_permission
+    @post = Post.find(params[:id])
+    @group = @post.group
+    if current_user != @post.user
+      redirect_to root_path, alert: 'You have no permission.'
+    end
+  end
+
+
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit]
+  before_action :find_post_and_check_permission, only: [:edit, :update, :destroy]
+  def index
+    @posts = current_user.posts.recent
+  end
+
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -17,8 +22,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path,notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to account_posts_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
   end
+
+  def find_post_and_check_permission
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path, alert: 'You have no permission.'
+    end
+  end
+
 end
